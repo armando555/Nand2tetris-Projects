9ceff6d6b2f9181c986a35c9fab36dbd532044a5
Armando Ríos Gallego <53227034+armando555@users.noreply.github.com>
2021-03-01 17:35:23 -0500

    multiplicar 100%

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,19 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+@16384
+D=A
+@r1
+M=D//GUARDO EL VALOR DONDE EMPIEZA LA PANTALLA EN 16
+@24575
+D=A
+@r2
+M=D//GUARDO EL VALOR DONDE TERMINA LA PANTALLA EN 17
+
+//poner negro
+@r1
+A=M
+D=A+1
+M=-1
+@r1
+M=D
--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -10,3 +10,41 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+@2
+M=0
+@0
+D=M
+@27//POR SI ES CERO
+D;JEQ
+@r0
+M=D//guardo r0 (3)
+@1
+D=M
+@27
+D;JEQ//POR SI ES CERO
+@r1
+M=D//guardo r1 (2)
+@r3
+D=M// obtengo el D antiguo
+@r0 
+A=M // A tiene a 3
+D=D+A //D = 0+3
+@r3
+M=D// guardo la suma 
+@r1
+D=M
+D=D-1
+M=D//disminuyo el contador
+@14
+D;JGT// si el contador es mayor que 0 vuelvo a la 8
+@r3
+D=M
+M=0
+@2// saco el resultado del producto y lo guardo en ram(2)
+M=D
+@r1
+M=0
+@r0
+M=0
+@34
+0;JMP
