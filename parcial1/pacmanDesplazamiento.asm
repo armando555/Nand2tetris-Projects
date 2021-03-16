696b085f2a58dd30959773f83c013f6c0e96e734
Armando Ríos Gallego <53227034+armando555@users.noreply.github.com>
2021-03-15 20:36:10 -0500

    PARCIAL 100%

--- parcial1/pacmanDesplazamiento.asm
+++ parcial1/pacmanDesplazamiento.asm
@@ -1,311 +1,433 @@
-@16384
-D=A
-@r2
-M=D//GUARDO POSICIÓN INICIAL DE PANTALLA
-@r3
-M=D//RESPALDO DE INICIO DE PANTALLA
-@16384
-D=A
-@r4
-M=D//GUARDO POSICIÓN FINAL DE PANTALLA
-@r5
-M=D//copia de la ultima posición
-@24576 // teclado
-D=M // guardo el dato del teclado
-@r6
-M=D //guardo el teclado por si algo
-@132//FLECHA DERECHA
-D=D-A
-@299//--------------------------------------------------------------------------------------------------------------DEBO ESTAR CAMBIANDO ESTE---------------------------------------------------------------------------------------------------------------------------------------------------
-D;JEQ
-@1984//guardo el valor de la linea para ese esta tabla---------------------------------------------------------------ESTE ES ABRIR BOCA--------------------------------------------------------------------------------------------------
-D=A
-@r2//obtengo la posición y le pongo el valor de esa linea
-A=M
-M=D
-@32//sumo 32 para cambiar de linea
-D=A
-@r2
-D=D+M
-M=D//actualicé el contador de posiciones
-@8176//guardo el valor de la linea para ese esta tabla
-D=A
-@r2//obtengo la posición y le pongo el valor de esa linea
-A=M
-M=D
-@32//sumo 32 para cambiar de linea
-D=A
-@r2
-D=D+M
-M=D
-@16376//guardo el valor de la linea para ese esta tabla
-D=A
-@r2//obtengo la posición y le pongo el valor de esa linea
-A=M
-M=D
-@32//sumo 32 para cambiar de linea
-D=A
-@r2
-D=D+M
-M=D
-@16376//guardo el valor de la linea para ese esta tabla
-D=A
-@r2//obtengo la posición y le pongo el valor de esa linea
-A=M
-M=D
-@32//sumo 32 para cambiar de linea
-D=A
-@r2
-D=D+M
-M=D
-@4092//guardo el valor de la linea para ese esta tabla
-D=A
-@r2//obtengo la posición y le pongo el valor de esa linea
-A=M
-M=D
-@32//sumo 32 para cambiar de linea
-D=A
-@r2
-D=D+M
-M=D
-@508//guardo el valor de la linea para ese esta tabla
-D=A
-@r2//obtengo la posición y le pongo el valor de esa linea
-A=M
-M=D
-@32//sumo 32 para cambiar de linea
-D=A
-@r2
-D=D+M
-M=D
-@60//guardo el valor de la linea para ese esta tabla
-D=A
-@r2//obtengo la posición y le pongo el valor de esa linea
-A=M
-M=D
-@32//sumo 32 para cambiar de linea
-D=A
-@r2
-D=D+M
-M=D
-@508//guardo el valor de la linea para ese esta tabla
-D=A
-@r2//obtengo la posición y le pongo el valor de esa linea
-A=M
-M=D
-@32//sumo 32 para cambiar de linea
-D=A
-@r2
-D=D+M
-M=D
-@4092//guardo el valor de la linea para ese esta tabla
-D=A
-@r2//obtengo la posición y le pongo el valor de esa linea
-A=M
-M=D
-@32//sumo 32 para cambiar de linea
-D=A
-@r2
-D=D+M
-M=D
-@16376//guardo el valor de la linea para ese esta tabla
-D=A
-@r2//obtengo la posición y le pongo el valor de esa linea
-A=M
-M=D
-@32//sumo 32 para cambiar de linea
-D=A
-@r2
-D=D+M
-M=D
-@16376//guardo el valor de la linea para ese esta tabla
-D=A
-@r2//obtengo la posición y le pongo el valor de esa linea
-A=M
-M=D
-@32//sumo 32 para cambiar de linea
-D=A
-@r2
-D=D+M
-M=D
-@8176//guardo el valor de la linea para ese esta tabla
-D=A
-@r2//obtengo la posición y le pongo el valor de esa linea
-A=M
-M=D
-@32//sumo 32 para cambiar de linea
-D=A
-@r2
-D=D+M
-M=D
-@1984//guardo el valor de la linea para ese esta tabla
-D=A
-@r2//obtengo la posición y le pongo el valor de esa linea
-A=M
-M=D
-@32//sumo 32 para cambiar de linea
-D=A
-@r2
-D=D+M
-M=D
-@0
-0;JMP//------------------------------------------------------------------------------------ESTE ES CERRAR BOCA-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-@1984//guardo el valor de la linea para ese esta tabla
-D=A
-@r2//obtengo la posición y le pongo el valor de esa linea
-A=M
-M=D
-@32//sumo 32 para cambiar de linea
-D=A
-@r2
-D=D+M
-M=D//actualicé el contador de posiciones
-@8176//guardo el valor de la linea para ese esta tabla
-D=A
-@r2//obtengo la posición y le pongo el valor de esa linea
-A=M
-M=D
-@32//sumo 32 para cambiar de linea
-D=A
-@r2
-D=D+M
-M=D
-@16376//guardo el valor de la linea para ese esta tabla
-D=A
-@r2//obtengo la posición y le pongo el valor de esa linea
-A=M
-M=D
-@32//sumo 32 para cambiar de linea
-D=A
-@r2
-D=D+M
-M=D
-@16376//guardo el valor de la linea para ese esta tabla
-D=A
-@r2//obtengo la posición y le pongo el valor de esa linea
-A=M
-M=D
-@32//sumo 32 para cambiar de linea
-D=A
-@r2
-D=D+M
-M=D
-@32764//guardo el valor de la linea para ese esta tabla
-D=A
-@r2//obtengo la posición y le pongo el valor de esa linea
-A=M
-M=D
-@32//sumo 32 para cambiar de linea
-D=A
-@r2
-D=D+M
-M=D
-@32764//guardo el valor de la linea para ese esta tabla
-D=A
-@r2//obtengo la posición y le pongo el valor de esa linea
-A=M
-M=D
-@32//sumo 32 para cambiar de linea
-D=A
-@r2
-D=D+M
-M=D
-@32764//guardo el valor de la linea para ese esta tabla
-D=A
-@r2//obtengo la posición y le pongo el valor de esa linea
-A=M
-M=D
-@32//sumo 32 para cambiar de linea
-D=A
-@r2
-D=D+M
-M=D
-@32764//guardo el valor de la linea para ese esta tabla
-D=A
-@r2//obtengo la posición y le pongo el valor de esa linea
-A=M
-M=D
-@32//sumo 32 para cambiar de linea
-D=A
-@r2
-D=D+M
-M=D
-@32764//guardo el valor de la linea para ese esta tabla
-D=A
-@r2//obtengo la posición y le pongo el valor de esa linea
-A=M
-M=D
-@32//sumo 32 para cambiar de linea
-D=A
-@r2
-D=D+M
-M=D
-@16376//guardo el valor de la linea para ese esta tabla
-D=A
-@r2//obtengo la posición y le pongo el valor de esa linea
-A=M
-M=D
-@32//sumo 32 para cambiar de linea
-D=A
-@r2
-D=D+M
-M=D
-@16376//guardo el valor de la linea para ese esta tabla
-D=A
-@r2//obtengo la posición y le pongo el valor de esa linea
-A=M
-M=D
-@32//sumo 32 para cambiar de linea
-D=A
-@r2
-D=D+M
-M=D
-@8176//guardo el valor de la linea para ese esta tabla
-D=A
-@r2//obtengo la posición y le pongo el valor de esa linea
-A=M
-M=D
-@32//sumo 32 para cambiar de linea
-D=A
-@r2
-D=D+M
-M=D
-@1984//guardo el valor de la linea para ese esta tabla
-D=A
-@r2//obtengo la posición y le pongo el valor de esa linea
-A=M
-M=D
-@32//sumo 32 para cambiar de linea
-D=A
-@r2
-D=D+M
-M=D
-@0//----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-0;JMP
-@r4//-------------------------------------------------------------------------------------------------------------------------PONER EN BLANCO//-------------------------------------
-A=M
-D=A+1
-M=0
-@r4
-M=D
-@24575
-D=A
-@r4
-A=M
-D=A-D
-@284//-------------------------------------------------------------------------------------------------------------------DEBO ESTAR CAMBIANDO ESTE----------------------------------------------------------------------------------------------------------------------------------------------
-D;JLT
-//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-0;JMP//acabo poner en blanco
-@r3//-------------------------------------------------------------------------------------------------------------------- FLECHA DERECHA CODIGO--------------------------------------------------------------------------------------
-D=M
-D=D+1
-M=D
-@r2
-M=D
-@284
-0;JMP
-@26
-0;JMP
-
+(INICIO)
+    @16384
+    D=A
+    @tempPosicion
+    M=D
+    @tempPosicionNew
+    M=D
+    @PACMANABIERTO
+    0;JMP
+(TECLA)
+    @24576
+    D=M
+    @tempTecla
+    M=D
+    @132
+    D=D-A
+    @TECLA
+    D;JNE
+    @MOVERDERECHA
+    0;JMP
+    (REGRESARDIBUJO)
+    (SOLTARTECLA)
+    @24576
+    D=M
+    @tempTecla
+    M=D
+    @132
+    D=D-A
+    @SOLTARTECLA
+    D;JEQ
+    @estado
+    D=M-1
+    @PACMANABIERTO
+    D;JLT
+    @PACMANCERRADO
+    D;JGE
+(PACMANABIERTO)
+    @estado
+    M=0
+    @1984
+    D=A
+    @tempPosicion
+    A=M
+    M=D
+    @32
+    D=A
+    @tempPosicion
+    D=D+M
+    M=D
+    @8176
+    D=A
+    @tempPosicion
+    A=M
+    M=D
+    @32
+    D=A
+    @tempPosicion
+    D=D+M
+    M=D
+    @16376
+    D=A
+    @tempPosicion
+    A=M
+    M=D
+    @32
+    D=A
+    @tempPosicion
+    D=D+M
+    M=D
+    @16376
+    D=A
+    @tempPosicion
+    A=M
+    M=D
+    @32
+    D=A
+    @tempPosicion
+    D=D+M
+    M=D
+    @4092
+    D=A
+    @tempPosicion
+    A=M
+    M=D
+    @32
+    D=A
+    @tempPosicion
+    D=D+M
+    M=D
+    @508
+    D=A
+    @tempPosicion
+    A=M
+    M=D
+    @32
+    D=A
+    @tempPosicion
+    D=D+M
+    M=D
+    @60
+    D=A
+    @tempPosicion
+    A=M
+    M=D
+    @32
+    D=A
+    @tempPosicion
+    D=D+M
+    M=D
+    @508
+    D=A
+    @tempPosicion
+    A=M
+    M=D
+    @32
+    D=A
+    @tempPosicion
+    D=D+M
+    M=D
+    @4092
+    D=A
+    @tempPosicion
+    A=M
+    M=D
+    @32
+    D=A
+    @tempPosicion
+    D=D+M
+    M=D
+    @16376
+    D=A
+    @tempPosicion
+    A=M
+    M=D
+    @32
+    D=A
+    @tempPosicion
+    D=D+M
+    M=D
+    @16376
+    D=A
+    @tempPosicion
+    A=M
+    M=D
+    @32
+    D=A
+    @tempPosicion
+    D=D+M
+    M=D
+    @8176
+    D=A
+    @tempPosicion
+    A=M
+    M=D
+    @32
+    D=A
+    @tempPosicion
+    D=D+M
+    M=D
+    @1984
+    D=A
+    @tempPosicion
+    A=M
+    M=D
+    @32
+    D=A
+    @tempPosicion
+    D=D+M
+    M=D
+    @estado
+    M=1
+    @TECLA
+    0;JMP
+    //-----------------------------------------------------------
+    (PACMANCERRADO)
+    @estado
+    M=1
+    @1984
+    D=A
+    @tempPosicion
+    A=M
+    M=D
+    @32
+    D=A
+    @tempPosicion
+    D=D+M
+    M=D
+    @8176
+    D=A
+    @tempPosicion
+    A=M
+    M=D
+    @32
+    D=A
+    @tempPosicion
+    D=D+M
+    M=D
+    @16376
+    D=A
+    @tempPosicion
+    A=M
+    M=D
+    @32
+    D=A
+    @tempPosicion
+    D=D+M
+    M=D
+    @16376
+    D=A
+    @tempPosicion
+    A=M
+    M=D
+    @32
+    D=A
+    @tempPosicion
+    D=D+M
+    M=D
+    @32764
+    D=A
+    @tempPosicion
+    A=M
+    M=D
+    @32
+    D=A
+    @tempPosicion
+    D=D+M
+    M=D
+    @32764
+    D=A
+    @tempPosicion
+    A=M
+    M=D
+    @32
+    D=A
+    @tempPosicion
+    D=D+M
+    M=D
+    @32764
+    D=A
+    @tempPosicion
+    A=M
+    M=D
+    @32
+    D=A
+    @tempPosicion
+    D=D+M
+    M=D
+    @32764
+    D=A
+    @tempPosicion
+    A=M
+    M=D
+    @32
+    D=A
+    @tempPosicion
+    D=D+M
+    M=D
+    @32764
+    D=A
+    @tempPosicion
+    A=M
+    M=D
+    @32
+    D=A
+    @tempPosicion
+    D=D+M
+    M=D
+    @16376
+    D=A
+    @tempPosicion
+    A=M
+    M=D
+    @32
+    D=A
+    @tempPosicion
+    D=D+M
+    M=D
+    @16376
+    D=A
+    @tempPosicion
+    A=M
+    M=D
+    @32
+    D=A
+    @tempPosicion
+    D=D+M
+    M=D
+    @8176
+    D=A
+    @tempPosicion
+    A=M
+    M=D
+    @32
+    D=A
+    @tempPosicion
+    D=D+M
+    M=D
+    @1984
+    D=A
+    @tempPosicion
+    A=M
+    M=D
+    @32
+    D=A
+    @tempPosicion
+    D=D+M
+    M=D
+    @estado
+    M=0
+    @TECLA
+    0;JMP
+(CLEAN)
+    @tempPosicion
+    A=M-1
+    M=0
+    @32
+    D=A
+    @tempPosicion
+    D=D+M
+    M=D
+    @tempPosicion
+    A=M-1
+    M=0
+    @32
+    D=A
+    @tempPosicion
+    D=D+M
+    M=D
+    @tempPosicion
+    A=M-1
+    M=0
+    @32
+    D=A
+    @tempPosicion
+    D=D+M
+    M=D
+    @tempPosicion
+    A=M-1
+    M=0
+    @32
+    D=A
+    @tempPosicion
+    D=D+M
+    M=D
+    @tempPosicion
+    A=M-1
+    M=0
+    @32
+    D=A
+    @tempPosicion
+    D=D+M
+    M=D
+    @tempPosicion
+    A=M-1
+    M=0
+    @32
+    D=A
+    @tempPosicion
+    D=D+M
+    M=D
+    @tempPosicion
+    A=M-1
+    M=0
+    @32
+    D=A
+    @tempPosicion
+    D=D+M
+    M=D
+    @tempPosicion
+    A=M-1
+    M=0
+    @32
+    D=A
+    @tempPosicion
+    D=D+M
+    M=D
+    @tempPosicion
+    A=M-1
+    M=0
+    @32
+    D=A
+    @tempPosicion
+    D=D+M
+    M=D
+    @tempPosicion
+    A=M-1
+    M=0
+    @32
+    D=A
+    @tempPosicion
+    D=D+M
+    M=D
+    @tempPosicion
+    A=M-1
+    M=0
+    @32
+    D=A
+    @tempPosicion
+    D=D+M
+    M=D
+    @tempPosicion
+    A=M-1
+    M=0
+    @32
+    D=A
+    @tempPosicion
+    D=D+M
+    M=D
+    @tempPosicion
+    A=M-1
+    M=0
+    @32
+    D=A
+    @tempPosicion
+    D=D+M
+    M=D
+    @tempPosicionNew
+    D=M
+    @tempPosicion
+    M=D
+    @VOLVERDERECHA
+    0;JMP
+(MOVERDERECHA)
+    @tempPosicionNew
+    D=M
+    MD=D+1
+    @tempPosicion
+    M=D
+    @CLEAN
+    0;JMP
+    (VOLVERDERECHA)
+    @REGRESARDIBUJO
+    0;JMP
